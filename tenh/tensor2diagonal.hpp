32a5b35f262d9ef3a8078559f9f8e4ebf326dcf0
Ted Nitz <tnitz@leapmotion.com>
2013-04-04 11:48:00 -0700

    Fix comments inherited from Tensor2Symmetric_t, and break lines at 120 characters.

--- tenh/tensor2diagonal.hpp
+++ tenh/tensor2diagonal.hpp
@@ -15,7 +15,7 @@
 
 namespace Tenh {
 
-// symmetric 2-tensor (it is equal to its transpose)
+// diagonal 2-tensor
 template <typename Factor1_, typename Factor2_, typename Derived_ = NullType>
 struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
                                            ((Factor1_::DIM < Factor2_::DIM) ? Factor1_::DIM : Factor2_::DIM),
@@ -23,7 +23,8 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
                                                                   Tensor2Diagonal_t<Factor1_,Factor2_,Derived_>,
                                                                   Derived_>::T>
 {
-    enum { FACTOR_SCALAR_TYPES_ARE_EQUAL = Lvd::Meta::Assert<Lvd::Meta::TypesAreEqual<typename Factor1_::Scalar,typename Factor2_::Scalar>::v>::v };
+    enum { FACTOR_SCALAR_TYPES_ARE_EQUAL = 
+        Lvd::Meta::Assert<Lvd::Meta::TypesAreEqual<typename Factor1_::Scalar,typename Factor2_::Scalar>::v>::v };
 
     typedef Vector_t<typename Factor1_::Scalar,
                      (Factor1_::DIM < Factor2_::DIM ? Factor1_::DIM : Factor2_::DIM),
@@ -81,12 +82,6 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
 //         return SourceIndexType(rowcol_index_to_contiguous_index(row, col));
 //     }
 
-    // TODO: because Factor1 and Factor2 are identical, it doesn't make sense to
-    // have a type coercion to either one unless they are 1-dimensional, in which case
-    // it can be a type coercion to the Scalar type.
-
-    // TODO: because the diagonal is indexed contiguously last, there is an easy type coercion to Tensor2Diagonal_t
-
     using Parent::operator[];
 
     // using two indices in a Tensor2Diagonal_t is breaking apart the Index type and using it
@@ -113,32 +108,39 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
     // this override of the Parent's operator() is necessary so that the expression template
     // knows that the operand is actually a Tensor2_t.
     template <char SYMBOL>
-    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,FORCE_CONST> operator () (
-        NamedIndex_t<Derived,SYMBOL> const &) const
+    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,FORCE_CONST>
+        operator () (NamedIndex_t<Derived,SYMBOL> const &) const
     {
         return expr<SYMBOL>();
     }
+    
     template <char SYMBOL>
-    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,DONT_FORCE_CONST> operator () (
-        NamedIndex_t<Derived,SYMBOL> const &)
+    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,DONT_FORCE_CONST>
+        operator () (NamedIndex_t<Derived,SYMBOL> const &)
     {
         return expr<SYMBOL>();
     }
+    
     // the corresponding outer product example here would be
     // u.expr<'i'>() * v.expr<'j'>()
     // this override of the Parent's operator() is necessary so that the expression template
     // knows that the operand is actually a Tensor2_t.
     template <char SYMBOL>
-    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,FORCE_CONST> expr () const
+    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,FORCE_CONST>
+        expr () const
     {
         Lvd::Meta::Assert<(SYMBOL != '\0')>();
-        return ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,FORCE_CONST>(this->as_derived());
+        return ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,
+                                                  EmptyTypeList,FORCE_CONST>(this->as_derived());
     }
+    
     template <char SYMBOL>
-    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,DONT_FORCE_CONST> expr ()
+    ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,DONT_FORCE_CONST>
+        expr ()
     {
         Lvd::Meta::Assert<(SYMBOL != '\0')>();
-        return ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,EmptyTypeList,DONT_FORCE_CONST>(this->as_derived());
+        return ExpressionTemplate_IndexedObject_t<Derived,TypeList_t<NamedIndex_t<Derived,SYMBOL> >,
+                                                  EmptyTypeList,DONT_FORCE_CONST>(this->as_derived());
     }
 
     // a 2-tensor can be indexed by the pair of factor indices (Factor1::Index, Factor2::Index)
@@ -162,6 +164,7 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
     {
         return expr<FACTOR1_SYMBOL,FACTOR2_SYMBOL>();
     }
+    
     template <char FACTOR1_SYMBOL, char FACTOR2_SYMBOL>
     ExpressionTemplate_IndexedObject_t<Derived,
                                        typename TypeTuple_t<
@@ -181,6 +184,7 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
     {
         return expr<FACTOR1_SYMBOL,FACTOR2_SYMBOL>();
     }
+    
     // the 2-index analog of expr<SYMBOL>()
     template <char FACTOR1_SYMBOL, char FACTOR2_SYMBOL>
     ExpressionTemplate_IndexedObject_t<Derived,
@@ -213,6 +217,7 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
                                                   FORCE_CONST
                                                   >(this->as_derived());
     }
+    
     template <char FACTOR1_SYMBOL, char FACTOR2_SYMBOL>
     ExpressionTemplate_IndexedObject_t<Derived,
                                        typename TypeTuple_t<
@@ -254,7 +259,8 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
         if (i1.is_at_end() || i2.is_at_end())
             throw std::invalid_argument("index/indices out of range");
 
-        if (!Factor1::component_corresponds_to_memory_location(i1) || !Factor2::component_corresponds_to_memory_location(i2))
+        if (!Factor1::component_corresponds_to_memory_location(i1)
+            || !Factor2::component_corresponds_to_memory_location(i2))
             return Scalar(0);
 
         CompoundIndex c(Factor1::vector_index_of(i1), Factor2::vector_index_of(i2));
@@ -265,6 +271,7 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
                Factor2::scalar_factor_for_component(i2) *
                operator[](vector_index_of(c));
     }
+    
     // write 2-tensor components -- will throw if a component doesn't correspond to a memory location
     // Index1 could be Factor1::Index or Factor1::CompoundIndex (checked by its use in the other functions)
     // Index2 could be Factor2::Index or Factor2::CompoundIndex (checked by its use in the other functions)
@@ -274,7 +281,8 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
         if (i1.is_at_end() || i2.is_at_end())
             throw std::invalid_argument("index/indices out of range");
 
-        if (!Factor1::component_corresponds_to_memory_location(i1) || !Factor2::component_corresponds_to_memory_location(i2))
+        if (!Factor1::component_corresponds_to_memory_location(i1)
+            || !Factor2::component_corresponds_to_memory_location(i2))
             throw std::invalid_argument("this tensor component is not writable");
 
         CompoundIndex c(Factor1::vector_index_of(i1), Factor2::vector_index_of(i2));
@@ -282,16 +290,20 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
             throw std::invalid_argument("this tensor component is not writable");
 
         // write to the component, but divide through by the total scale factor for the component.
-        operator[](vector_index_of(c)) = s / (Factor1::scalar_factor_for_component(i1) * Factor2::scalar_factor_for_component(i2));
+        operator[](vector_index_of(c)) = s / (Factor1::scalar_factor_for_component(i1) 
+                                              * Factor2::scalar_factor_for_component(i2));
     }
+    
     using Parent::component_corresponds_to_memory_location;
     using Parent::scalar_factor_for_component;
     using Parent::vector_index_of;
-    // the diagonal is not stored in memory -- all components are zero.
+
     static bool component_corresponds_to_memory_location (CompoundIndex const &c)
     {
-        return c.template el<0>().value() == c.template el<1>().value(); // only diagonal elements correspond to memory locations
+        // only diagonal elements correspond to memory locations
+        return c.template el<0>().value() == c.template el<1>().value(); 
     }
+    
     // the diagonal components have a scale factor of 1, all others have 0
     static Scalar scalar_factor_for_component (CompoundIndex const &c)
     {
@@ -300,10 +312,12 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
         else // off diagonal
             return Scalar(0); // not actually used, but is here for completeness
     }
+    
     // this should return iff component_corresponds_to_memory_location(c) returns true.
     static Index vector_index_of (CompoundIndex const &c)
     {
-        return Index::range_unchecked(rowcol_index_to_contiguous_index(c.template el<0>().value(), c.template el<1>().value()));
+        return Index::range_unchecked(rowcol_index_to_contiguous_index(c.template el<0>().value(),
+                                                                       c.template el<1>().value()));
     }
 
     static std::string type_as_string ()
@@ -317,7 +331,8 @@ struct Tensor2Diagonal_t : public Vector_t<typename Factor1_::Scalar,
         if (Lvd::Meta::TypesAreEqual<Derived_,NullType>::v)
             return "Tensor2Diagonal_t<" + TypeStringOf_t<Factor1>::eval() + ',' + TypeStringOf_t<Factor2>::eval() + '>';
         else
-            return "Tensor2Diagonal_t<" + TypeStringOf_t<Factor1>::eval() + ',' + TypeStringOf_t<Factor2>::eval() + ',' + TypeStringOf_t<Derived>::eval() + '>';
+            return "Tensor2Diagonal_t<" + TypeStringOf_t<Factor1>::eval() + ',' + TypeStringOf_t<Factor2>::eval() + ','
+                    + TypeStringOf_t<Derived>::eval() + '>';
     }
 
 private:
