c18bed90490a15c11b56766c475fbe0818ad0876
Ted Nitz <tnitz@leapmotion.com>
2013-04-15 11:08:13 -0700

    Fix Justification within the namespace.

--- tensorhell/standard/test_expressiontemplates.cpp
+++ tensorhell/standard/test_expressiontemplates.cpp
@@ -28,65 +28,65 @@ using namespace TestSystem;
 namespace Test {
 namespace ExpressionTemplates {
 
-    template <typename Scalar, Uint32 DIM>
-    void test_Tensor2Antisymmetric_t (Context const &context)
+template <typename Scalar, Uint32 DIM>
+void test_Tensor2Antisymmetric_t (Context const &context)
+{
+    typedef Tenh::Vector_t<Scalar,DIM> Vector;
+    typedef Tenh::Tensor2Antisymmetric_t<Vector> Tensor2Antisymmetric;
+
+    Tensor2Antisymmetric a(Tenh::Static<Tenh::WithoutInitialization>::SINGLETON);
+    Tensor2Antisymmetric b(Tenh::Static<Tenh::WithoutInitialization>::SINGLETON);
+    for (typename Tensor2Antisymmetric::Index i; i.is_not_at_end(); ++i)
     {
-        typedef Tenh::Vector_t<Scalar,DIM> Vector;
-        typedef Tenh::Tensor2Antisymmetric_t<Vector> Tensor2Antisymmetric;
-
-        Tensor2Antisymmetric a(Tenh::Static<Tenh::WithoutInitialization>::SINGLETON);
-        Tensor2Antisymmetric b(Tenh::Static<Tenh::WithoutInitialization>::SINGLETON);
-        for (typename Tensor2Antisymmetric::Index i; i.is_not_at_end(); ++i)
-        {
-            a[i] = i.value() + 1;
-            b[i] = sqrt(i.value()) + 5;
-        }
-
-        float hand_computed_value = 0.0f;
-        for (typename Tensor2Antisymmetric::MultiIndex i; i.is_not_at_end(); ++i)
-        {
-            hand_computed_value += a[i]*b[i];
-        }
-        
-        Tenh::TypedIndex_t<Tensor2Antisymmetric,'i'> i;
-        
-        assert_eq(hand_computed_value, a(i)*b(i));
-        
-        Tenh::TypedIndex_t<Vector,'j'> j;
-        Tenh::TypedIndex_t<Vector,'k'> k;
-        
-        assert_eq(hand_computed_value, a(j|k)*b(j|k));
+        a[i] = i.value() + 1;
+        b[i] = sqrt(i.value()) + 5;
     }
 
-    template <typename Scalar, Uint32 DIM>
-    void add_particular_tests (Directory *parent)
+    float hand_computed_value = 0.0f;
+    for (typename Tensor2Antisymmetric::MultiIndex i; i.is_not_at_end(); ++i)
     {
-        typedef Tenh::Vector_t<Scalar,DIM> V;
-        typedef Tenh::Tensor2Antisymmetric_t<V, V> Tensor2Antisymmetric;
-
-        Directory *tensor2antisymmetric
-            = new Directory(Tenh::TypeStringOf_t<Tensor2Antisymmetric>::eval(), parent);
-
-        LVD_ADD_NAMED_TEST_CASE_FUNCTION(tensor2antisymmetric, "test",
-            test_Tensor2Antisymmetric_t<Scalar, DIM>, RESULT_NO_ERROR);
-
+        hand_computed_value += a[i]*b[i];
     }
+    
+    Tenh::TypedIndex_t<Tensor2Antisymmetric,'i'> i;
+    
+    assert_eq(hand_computed_value, a(i)*b(i));
+    
+    Tenh::TypedIndex_t<Vector,'j'> j;
+    Tenh::TypedIndex_t<Vector,'k'> k;
+    
+    assert_eq(hand_computed_value, a(j|k)*b(j|k));
+}
 
-    template <typename Scalar>
-    void add_particular_tests_for_scalar (Directory *parent)
-    {
-        add_particular_tests<Scalar,2>(parent);
-        add_particular_tests<Scalar,8>(parent);
-        add_particular_tests<Scalar,100>(parent);
-    }
+template <typename Scalar, Uint32 DIM>
+void add_particular_tests (Directory *parent)
+{
+    typedef Tenh::Vector_t<Scalar,DIM> V;
+    typedef Tenh::Tensor2Antisymmetric_t<V, V> Tensor2Antisymmetric;
 
-    void AddTests (Directory *parent)
-    {
-        Directory *expression_templates = new Directory("ExpressionTemplates", parent);
-        add_particular_tests_for_scalar<float>(expression_templates);
+    Directory *tensor2antisymmetric
+        = new Directory(Tenh::TypeStringOf_t<Tensor2Antisymmetric>::eval(), parent);
+
+    LVD_ADD_NAMED_TEST_CASE_FUNCTION(tensor2antisymmetric, "test",
+        test_Tensor2Antisymmetric_t<Scalar, DIM>, RESULT_NO_ERROR);
+
+}
+
+template <typename Scalar>
+void add_particular_tests_for_scalar (Directory *parent)
+{
+    add_particular_tests<Scalar,2>(parent);
+    add_particular_tests<Scalar,8>(parent);
+    add_particular_tests<Scalar,100>(parent);
+}
+
+void AddTests (Directory *parent)
+{
+    Directory *expression_templates = new Directory("ExpressionTemplates", parent);
+    add_particular_tests_for_scalar<float>(expression_templates);
 
 
-    }
+}
 
 } // end of namespace ExpressionTemplates
 } // end of namespace Test
