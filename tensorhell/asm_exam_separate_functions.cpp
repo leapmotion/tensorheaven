422cbc9f9cf9035ca2d314db58e9faddf0474771
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2013-04-19 19:51:38 -0700

    Added a "separate functions" file to the assembler exam build for looking at code in isolation.

---
+++ tensorhell/asm_exam_separate_functions.cpp
@@ -0,0 +1,29 @@
+#include "tenh/euclideanembedding.hpp"
+#include "tenh/tensor2.hpp"
+#include "tenh/tensor2symmetric.hpp"
+#include "tenh/vector.hpp"
+
+using namespace Tenh;
+using namespace std;
+
+typedef Vector_t<float,1000> V;
+typedef typename V::WithStandardEuclideanBasis EV;
+// typedef Tensor2_t<V,V> T;
+// typedef Tensor2Symmetric_t<V,V> S;
+
+float full_contraction_with_euclidean_embedding (EV const &u, V const &v)
+{
+    TypedIndex_t<EV,'i'> i;
+    TypedIndex_t<V,'j'> j;
+    EuclideanEmbedding_t<V> e; // because V uses StandardEuclideanBasis, this should be the identity
+    // hopefully the code should simplify to something like a hand-written dot product
+    return u(i)*e(i|j)*v(j);
+}
+
+float hand_written_inner_product (EV const &u, V const &v)
+{
+    float retval(0);
+    for (Uint32 i = 0; i < 1000; ++i)
+        retval += u[typename EV::Index(i, DONT_CHECK_RANGE)] * v[typename V::Index(i, DONT_CHECK_RANGE)];
+    return retval;
+}
