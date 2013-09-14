e7596ddf426fe58202c4d3795637a0333e1cd437
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2013-09-13 22:44:54 -0700

    Added a way to do pretty-printing of TypeStringOf_t<T_> types, with optional "shortifying" to make the type strings less specific and massive.

--- tenh/meta/typestringof.hpp
+++ tenh/meta/typestringof.hpp
@@ -6,9 +6,9 @@
 #ifndef TENH_META_TYPESTRINGOF_HPP_
 #define TENH_META_TYPESTRINGOF_HPP_
 
-#include <string>
-// #include <sstream> // include this only if AS_STRING is used
 #include <complex>
+#include <ostream>
+#include <string>
 
 #include "tenh/core.hpp"
 
@@ -34,6 +34,88 @@ template <> struct TypeStringOf_t<float> { static std::string eval () { return "
 template <> struct TypeStringOf_t<double> { static std::string eval () { return "double"; } };
 template <> struct TypeStringOf_t<long double> { static std::string eval () { return "long double"; } };
 template <> struct TypeStringOf_t<std::string> { static std::string eval () { return "std::string"; } };
+
+// used as a tag for "pretty" printing of type strings
+template <typename TypeStringOf_, Uint32 SHORTIFY_DEPTH_ = 0>
+struct Pretty
+{
+    typedef TypeStringOf_ TypeStringOf;
+    static Uint32 const SHORTIFY_DEPTH = SHORTIFY_DEPTH_;
+};
+
+// shortify_depth is the depth at which "template arguments" are replaced with an ellipsis;
+// a shortify_depth of 0 means that no ellipsis-collapsing will be done, and e.g. a
+// shortify_depth of 1 will produce something like FooBar<...>.
+void print_pretty_typestring (std::ostream &out,
+                              std::string const &typestring,
+                              Uint32 indent_level,
+                              Uint32 indent_size = 4,
+                              Uint32 shortify_depth = 0)
+{
+    Uint32 template_argument_depth = 0;
+    for (std::string::const_iterator it = typestring.begin(), it_end = typestring.end(); it != it_end; ++it)
+    {
+        char const &c = *it;
+        switch (c)
+        {
+            case '<':
+                ++template_argument_depth;
+                // we're shortified, so print the shortified "template arguments"
+                if (shortify_depth > 0 && template_argument_depth == shortify_depth)
+                {
+                    out << "<...>";
+                }
+                else
+                {
+                    out << "<\n";
+                    ++indent_level;
+                    for (Uint32 i = 0; i < indent_level; ++i)
+                        out << std::string(indent_size, ' ');
+                }
+                break;
+
+            case ',':
+                // only print stuff if we're not shortified
+                if (shortify_depth == 0 || template_argument_depth < shortify_depth)
+                {
+                    out << ",\n";
+                    for (Uint32 i = 0; i < indent_level; ++i)
+                        out << std::string(indent_size, ' ');
+                }
+                break;
+
+            case '>':
+                // presumably the < and > brackets will be balanced, but in case they're not...
+                if (indent_level > 0)
+                    --indent_level;
+                // only print stuff if we're not shortified
+                if (shortify_depth == 0 || template_argument_depth < shortify_depth)
+                {
+                    out << '\n';
+                    for (Uint32 i = 0; i < indent_level; ++i)
+                        out << std::string(indent_size, ' ');
+                    out << '>';
+                }
+                if (template_argument_depth)
+                    --template_argument_depth;
+                break;
+
+            default:
+                // only print stuff if we're not shortified
+                if (shortify_depth == 0 || template_argument_depth < shortify_depth)
+                    out << c;
+                break;
+        }
+    }
+}
+
+// does nicely indented formatting
+template <typename T_, Uint32 SHORTIFY_DEPTH_>
+std::ostream &operator << (std::ostream &out, Pretty<TypeStringOf_t<T_>,SHORTIFY_DEPTH_> const &)
+{
+    print_pretty_typestring(out, TypeStringOf_t<T_>::eval(), 0, 4, SHORTIFY_DEPTH_);
+    return out;
+}
 
 } // end of namespace Tenh
 
