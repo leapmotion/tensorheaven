2993d29b8fec796587bf4e570a43fd3368c8f7bf
Ted Nitz <tnitz@leapmotion.com>
2013-10-30 15:01:11 -0700

    Update Eigen SVD support to work with the "new" tenh interfaces.

--- tenh/interop/eigen_svd.hpp
+++ tenh/interop/eigen_svd.hpp
@@ -16,70 +16,83 @@
 
 namespace Tenh {
 
-template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayType_>
+// We must have t(i|l) == u(i|j) * s(j|k) * v(l|k), i.e. t = u * s * v^T. Also u, and v are square matrices.
+template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayTypeT_, typename UseArrayTypeU_, typename UseArrayTypeS_, typename UseArrayTypeV_>
 void SVD_of_2tensor (ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
                                                                            TypeList_t<Factor2_> > >,
                                         Scalar_,
-                                        UseArrayType_> const &t,
+                                        UseArrayTypeT_> const &t,
                      ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
-                                                                           TypeList_t<TODO THINK ABOUT THIS> > >,
+                                                                           TypeList_t<typename DualOf_f<Factor1_>::T > > >,
                                         Scalar_,
-                                        UseArrayType_> const &u,
-                     ImplementationOf_t<Diagonal2TensorProductOfBasedVectorSpaces_c<TODO THINK ABOUT THIS, THINK ABOUT>,
+                                        UseArrayTypeU_> &u,
+                     ImplementationOf_t<Diagonal2TensorProductOfBasedVectorSpaces_c<Factor1_,typename DualOf_f<Factor2_>::T >,
                                         Scalar_,
-                                        UseArrayType_> const &s,
-                     ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<TODO THINK ABOUT THIS,
-                                                                           TypeList_t<Factor2_> > >,
+                                        UseArrayTypeS_> &s,
+                     ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor2_,
+                                                                           TypeList_t<typename DualOf_f<Factor2_>::T > > >,
                                         Scalar_,
-                                        UseArrayType_> const &v)
+                                        UseArrayTypeV_> &v)
 {
-}
-
-// SVD with u and v matrices; the singular values are in s; t = u * s * v^T
-template <typename Factor1, typename Factor2, typename Basis, typename Derived>
-void SVD_of_Tensor2 (Tensor2_t<Factor1,Factor2,Basis,Derived> const &t,
-                     Tensor2_t<Factor1,Factor1> &u,
-                     Tensor2Diagonal_t<Factor1,Factor2> &s,
-                     Tensor2_t<Factor2,Factor2> &v)
-{
-    typedef Eigen::Matrix<typename Factor1::Scalar,Factor1::DIM,Factor2::DIM,Eigen::RowMajor> EigenMatrix;
-    Eigen::JacobiSVD<EigenMatrix> svd(euclideanly_embedded_EigenMatrix_from(t).jacobiSvd(Eigen::ComputeFullU|Eigen::ComputeFullV));
+    typedef Eigen::Matrix<Scalar_,DimensionOf_f<Factor1_>::V,DimensionOf_f<Factor2_>::V,Eigen::RowMajor> EigenMatrix;
+    Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeFullU|Eigen::ComputeFullV));
     memcpy(u.pointer_to_allocation(), &svd.matrixU()(0,0), u.allocation_size_in_bytes());
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
     memcpy(v.pointer_to_allocation(), &svd.matrixV()(0,0), v.allocation_size_in_bytes());
 }
 
 // SVD with u matrix but no v matrix; the singular values are in s.
-template <typename Factor1, typename Factor2, typename Basis, typename Derived>
-void SVD_of_Tensor2 (Tensor2_t<Factor1,Factor2,Basis,Derived> const &t,
-                     Tensor2_t<Factor1,Factor1> &u,
-                     Tensor2Diagonal_t<Factor1,Factor2> &s)
+template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayTypeT_, typename UseArrayTypeU_, typename UseArrayTypeS_, typename UseArrayTypeV_>
+void SVD_of_2tensor (ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
+                                                                           TypeList_t<Factor2_> > >,
+                                        Scalar_,
+                                        UseArrayTypeT_> const &t,
+                     ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
+                                                                           TypeList_t<typename DualOf_f<Factor1_>::T > > >,
+                                        Scalar_,
+                                        UseArrayTypeU_> &u,
+                     ImplementationOf_t<Diagonal2TensorProductOfBasedVectorSpaces_c<Factor1_,typename DualOf_f<Factor2_>::T >,
+                                        Scalar_,
+                                        UseArrayTypeS_> &s)
 {
-    typedef Eigen::Matrix<typename Factor1::Scalar,Factor1::DIM,Factor2::DIM,Eigen::RowMajor> EigenMatrix;
-    Eigen::JacobiSVD<EigenMatrix> svd(euclideanly_embedded_EigenMatrix_from(t).jacobiSvd(Eigen::ComputeFullU));
+    typedef Eigen::Matrix<Scalar_,DimensionOf_f<Factor1_>::V,DimensionOf_f<Factor2_>::V,Eigen::RowMajor> EigenMatrix;
+    Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeFullU));
     memcpy(u.pointer_to_allocation(), &svd.matrixU()(0,0), u.allocation_size_in_bytes());
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
 }
 
 // SVD with v matrix but no u matrix; the singular values are in s.
-template <typename Factor1, typename Factor2, typename Basis, typename Derived>
-void SVD_of_Tensor2 (Tensor2_t<Factor1,Factor2,Basis,Derived> const &t,
-                     Tensor2Diagonal_t<Factor1,Factor2> &s,
-                     Tensor2_t<Factor2,Factor2> &v)
+template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayTypeT_, typename UseArrayTypeU_, typename UseArrayTypeS_, typename UseArrayTypeV_>
+void SVD_of_2tensor (ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
+                                                                           TypeList_t<Factor2_> > >,
+                                        Scalar_,
+                                        UseArrayTypeT_> const &t,
+                     ImplementationOf_t<Diagonal2TensorProductOfBasedVectorSpaces_c<Factor1_,typename DualOf_f<Factor2_>::T >,
+                                        Scalar_,
+                                        UseArrayTypeS_> &s,
+                     ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor2_,
+                                                                           TypeList_t<typename DualOf_f<Factor2_>::T > > >,
+                                        Scalar_,
+                                        UseArrayTypeV_> &v)
 {
-    typedef Eigen::Matrix<typename Factor1::Scalar,Factor1::DIM,Factor2::DIM,Eigen::RowMajor> EigenMatrix;
-    Eigen::JacobiSVD<EigenMatrix> svd(euclideanly_embedded_EigenMatrix_from(t).jacobiSvd(Eigen::ComputeFullU));
+    typedef Eigen::Matrix<Scalar_,DimensionOf_f<Factor1_>::V,DimensionOf_f<Factor2_>::V,Eigen::RowMajor> EigenMatrix;
+    Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeFullV));
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
     memcpy(v.pointer_to_allocation(), &svd.matrixV()(0,0), v.allocation_size_in_bytes());
 }
 
 // SVD with no u or v matrix; the singular values are in s.
-template <typename Factor1, typename Factor2, typename Basis, typename Derived>
-void SVD_of_Tensor2 (Tensor2_t<Factor1,Factor2,Basis,Derived> const &t,
-                     Tensor2Diagonal_t<Factor1,Factor2> &s)
+template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayTypeT_, typename UseArrayTypeU_, typename UseArrayTypeS_, typename UseArrayTypeV_>
+void SVD_of_2tensor (ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
+                                                                           TypeList_t<Factor2_> > >,
+                                        Scalar_,
+                                        UseArrayTypeT_> const &t,
+                     ImplementationOf_t<Diagonal2TensorProductOfBasedVectorSpaces_c<Factor1_,typename DualOf_f<Factor2_>::T >,
+                                        Scalar_,
+                                        UseArrayTypeS_> &s)
 {
-    typedef Eigen::Matrix<typename Factor1::Scalar,Factor1::DIM,Factor2::DIM,Eigen::RowMajor> EigenMatrix;
-    Eigen::JacobiSVD<EigenMatrix> svd(euclideanly_embedded_EigenMatrix_from(t).jacobiSvd());
+    typedef Eigen::Matrix<Scalar_,DimensionOf_f<Factor1_>::V,DimensionOf_f<Factor2_>::V,Eigen::RowMajor> EigenMatrix;
+    Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd());
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
 }
 
