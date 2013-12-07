340e966db72ab4753b97eb0a940389e1d7fcdb1b
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2013-12-07 14:59:21 -0800

    Added a prototype for compile-time generated lookup tables for metafunctions.  For whatever reason, clang is not generating the SymmetricIndex static constant values correctly, but GCC is.

---
+++ tensorhell/compile_time_generated_lookup_table.cpp
@@ -0,0 +1,256 @@
+#include <cassert>
+#include <iostream>
+
+#include "tenh/core.hpp"
+#include "tenh/meta/typestringof.hpp"
+
+using namespace std;
+
+typedef unsigned long long int uint;
+
+/*
+// assuming Function_f_<K_>::V is increasing w.r.t. K_, this returns the
+// K_ such that Function_f_<K_>::V <= N_ < Function_f_<K_+1>::V.
+template <uint N_, template <uint K_> class Function_f_>
+struct Largest_K_SuchThat_Function_K_LTE_N_f
+{
+private:
+
+};
+*/
+// prototype design goal(s):
+// - supply a metafunction and have it generate a static lookup table at compile time
+// - ideally this would work for non-integer datatypes, though this may require an
+//   index map, e.g. mapping the index type into a contiguous set of integers.
+//   call this index -> integer map the "hash" map, and require it to be injective.
+// - the problem with using a generic index is that it needs to be a type that is
+//   allowable both as a template parameter and as a runtime value.  so maybe two
+//   versions of the index type must be available, as well as two versions of the
+//   hash map -- one a compile-time TMP valued thing, and the other a runtime valued
+//   thing.
+
+// generates a lookup table for Function_f_ in range [START_, END_)
+template <template <uint N_> class Function_f_, uint START_, uint END_, typename Codomain_>
+struct LookupTable_t
+{
+    LookupTable_t ()
+        :
+        m_start_value(Function_f_<START_>::V),
+        m_body_lookup_table()
+    {
+        // make sure the range is valid
+        assert(START_ <= END_);
+        // make sure that each m_start_value is actually layed out contiguously in memory
+        assert(&m_start_value + 1 == m_body_lookup_table.as_array());
+    }
+    Codomain_ const *as_array () const
+    {
+        return &m_start_value;
+    }
+    Codomain_ operator [] (uint i) const
+    {
+        assert(START_ <= i && i < END_);
+        return as_array()[i - START_];
+    }
+private:
+    typedef LookupTable_t<Function_f_,START_+1,END_,Codomain_> BodyLookupTable;
+
+    Codomain_ const m_start_value;
+    BodyLookupTable m_body_lookup_table;
+};
+
+template <template <uint N_> class Function_f_, uint START_END_, typename Codomain_>
+struct LookupTable_t<Function_f_,START_END_,START_END_,Codomain_>
+{
+    Codomain_ const *as_array () const { return reinterpret_cast<Codomain_ const *>(this); }
+};
+
+// test metafunction
+template <uint N_> struct Factorial_f { static uint const V = N_ * Factorial_f<N_-1>::V; };
+template <> struct Factorial_f<0> { static uint const V = 1; };
+
+///////////////////////////////////////////////////////////////////////////////
+
+uint triangular_number (uint n) { return (n * (n + 1)) / 2; }
+
+struct SymmetricIndex
+{
+    uint i, j;
+    SymmetricIndex (uint i_, uint j_) : i(i_), j(j_) { assert (i >= j); }
+};
+
+uint symmetric_index_hash (SymmetricIndex const &s)
+{
+    return triangular_number(s.i) + s.j;
+}
+
+std::ostream &operator << (std::ostream &out, SymmetricIndex const &s)
+{
+    return out << "SymmetricIndex(" << s.i << ',' << s.j << ')';
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <uint N_>
+struct TriangularNumber_f
+{
+    static uint const V = (N_ * (N_ + 1)) / 2;
+};
+
+// here's a nontrivial index type
+template <uint I_, uint J_>
+struct SymmetricIndex_t
+{
+    // the indices must be non-increasing (lower triangular)
+    enum { _ = Tenh::Assert<(I_ >= J_)>::V };
+
+    static SymmetricIndex const V;
+
+    static std::string type_as_string () { return "SymmetricIndex_t<" + AS_STRING(I_) + ',' + AS_STRING(J_) + '>'; }
+};
+
+template <uint I_, uint J_>
+SymmetricIndex const SymmetricIndex_t<I_,J_>::V(SymmetricIndex(I_,J_));
+
+template <typename SymmetricIndex_> struct SymmetricIndexHash_f;
+
+template <uint I_, uint J_>
+struct SymmetricIndexHash_f<SymmetricIndex_t<I_,J_> >
+{
+    static uint const V = TriangularNumber_f<I_>::V + J_;
+};
+
+template <uint N_, uint K_>
+struct SymmetricIndexHashInverse_Helper_f
+{
+    // 0
+    // 1 2
+    // 3 4 5
+    // 6 7 8 9
+    // . . . . .
+    typedef typename Tenh::If_f<(N_ < K_),
+                                Tenh::Type_t<SymmetricIndex_t<K_-1,N_> >,
+                                SymmetricIndexHashInverse_Helper_f<N_-K_,K_+1> >::T::T T;
+};
+
+template <uint K_>
+struct SymmetricIndexHashInverse_Helper_f<0,K_>
+{
+    typedef SymmetricIndex_t<K_-1,0> T;
+};
+
+template <uint N_>
+struct SymmetricIndexHashInverse_f
+{
+    // 0
+    // 1 2
+    // 3 4 5
+    // 6 7 8 9
+    // . . . . .
+    typedef typename SymmetricIndexHashInverse_Helper_f<N_,1>::T T;
+    static SymmetricIndex const V;
+};
+
+template <uint N_>
+SymmetricIndex const SymmetricIndexHashInverse_f<N_>::V = SymmetricIndexHashInverse_f<N_>::T::V;
+
+int main (int argc, char **argv)
+{
+    cout << "0! = " << Factorial_f<0>::V << '\n';
+    cout << "1! = " << Factorial_f<1>::V << '\n';
+    cout << "2! = " << Factorial_f<2>::V << '\n';
+    cout << "3! = " << Factorial_f<3>::V << '\n';
+    cout << "4! = " << Factorial_f<4>::V << '\n';
+    cout << "5! = " << Factorial_f<5>::V << '\n';
+    cout << '\n';
+
+    {
+        typedef LookupTable_t<Factorial_f,0,20,uint> LookupTable;
+        LookupTable lookup;
+        for (uint i = 0; i < 20; ++i)
+            cout << "lookup(" << i << ") = " << lookup[i] << '\n';
+    }
+
+    typedef SymmetricIndex_t<2,1> S;
+#define PRINT_SYMMETRIC_INDEX_HASH(i,j) cout << "SymmetricIndexHash_f<" << i << "," << j << ">::V = " \
+                                             << SymmetricIndexHash_f<SymmetricIndex_t<i,j> >::V << '\n'
+    PRINT_SYMMETRIC_INDEX_HASH(0,0);
+    PRINT_SYMMETRIC_INDEX_HASH(1,0);
+    PRINT_SYMMETRIC_INDEX_HASH(1,1);
+    PRINT_SYMMETRIC_INDEX_HASH(2,0);
+    PRINT_SYMMETRIC_INDEX_HASH(2,1);
+    PRINT_SYMMETRIC_INDEX_HASH(2,2);
+    cout << '\n';
+
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<0>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<1>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<2>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<3>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<4>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<5>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<6>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<7>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<8>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<9>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<10>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<11>::T>())) << '\n';
+    cout << '\n';
+
+    cout << FORMAT_VALUE(SymmetricIndexHash_f<SymmetricIndexHashInverse_f<0>::T>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHash_f<SymmetricIndexHashInverse_f<1>::T>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHash_f<SymmetricIndexHashInverse_f<2>::T>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHash_f<SymmetricIndexHashInverse_f<3>::T>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHash_f<SymmetricIndexHashInverse_f<4>::T>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHash_f<SymmetricIndexHashInverse_f<5>::T>::V) << '\n';
+    cout << '\n';
+
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<SymmetricIndexHash_f<SymmetricIndex_t<0,0> >::V>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<SymmetricIndexHash_f<SymmetricIndex_t<1,0> >::V>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<SymmetricIndexHash_f<SymmetricIndex_t<1,1> >::V>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<SymmetricIndexHash_f<SymmetricIndex_t<2,0> >::V>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<SymmetricIndexHash_f<SymmetricIndex_t<2,1> >::V>::T>())) << '\n';
+    cout << FORMAT_VALUE((Tenh::type_string_of<SymmetricIndexHashInverse_f<SymmetricIndexHash_f<SymmetricIndex_t<2,2> >::V>::T>())) << '\n';
+    cout << '\n';
+
+    // cout << FORMAT_VALUE((SymmetricIndex_t<0,0>::V)) << '\n';
+    // cout << FORMAT_VALUE((SymmetricIndex_t<1,0>::V)) << '\n';
+    // cout << FORMAT_VALUE((SymmetricIndex_t<1,1>::V)) << '\n';
+    // cout << FORMAT_VALUE((SymmetricIndex_t<2,0>::V)) << '\n';
+    // cout << FORMAT_VALUE((SymmetricIndex_t<2,1>::V)) << '\n';
+    // cout << FORMAT_VALUE((SymmetricIndex_t<2,2>::V)) << '\n';
+    // cout << FORMAT_VALUE((SymmetricIndex_t<3,0>::V)) << '\n';
+    // cout << '\n';
+
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<0>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<1>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<2>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<3>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<4>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<5>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<6>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<7>::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<8>::V) << '\n';
+    cout << '\n';
+
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<0>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<1>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<2>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<3>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<4>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<5>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<6>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<7>::T::V) << '\n';
+    cout << FORMAT_VALUE(SymmetricIndexHashInverse_f<8>::T::V) << '\n';
+    cout << '\n';
+
+    {
+        static uint const LOOKUP_TABLE_SIZE = 40;
+        typedef LookupTable_t<SymmetricIndexHashInverse_f,0,LOOKUP_TABLE_SIZE,SymmetricIndex> LookupTable;
+        LookupTable lookup;
+        for (uint i = 0; i < LOOKUP_TABLE_SIZE; ++i)
+            cout << "lookup(" << i << ") = " << lookup[i] << '\n';
+    }
+
+
+    return 0;
+}
