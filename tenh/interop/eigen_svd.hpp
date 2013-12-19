7d863b5fd0ad16d5316a206e9f207d72c25f803e
Ted Nitz <tnitz@leapmotion.com>
2013-12-18 18:51:05 -0800

    Move eigen_svd.hpp to use Vector_i/Tensor_i as function parameters rather than ImplementationOf_t.

--- tenh/interop/eigen_svd.hpp
+++ tenh/interop/eigen_svd.hpp
@@ -31,84 +31,61 @@ public:
 template <typename T> struct SVDReturnTypesOf_m;
 
 // We must have t(i*l) == u(i*j) * s(j*k) * v(l*k), i.e. t = u * s * v^T. Also u, and v are square matrices.
-template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayTypeT_, typename Derived_>
-struct SVDReturnTypesOf_m<ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,TypeList_t<Factor2_> > >,Scalar_, UseArrayTypeT_, Derived_> >
+template <typename Factor1_, typename Factor2_>
+struct SVDReturnTypesOf_m<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,TypeList_t<Factor2_> > > >
 {
-    template <typename UseArrayTypeU_ = UseArrayTypeT_>
     struct OfU_f
     {
-    private:
-        enum { STATIC_ASSERT_IN_ENUM((!IsUseImmutableArray_f<UseArrayTypeU_>::V), MUST_NOT_BE_USE_IMMUTABLE_ARRAY) };
-    public:
-        typedef ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
-                                                                      TypeList_t<typename DualOf_f<Factor1_>::T > > >,
-                                   Scalar_,
-                                   UseArrayTypeU_> T;
+        typedef TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,
+                                                   TypeList_t<typename DualOf_f<Factor1_>::T > > > T;
     };
 
-    template <typename UseArrayTypeS_ = UseArrayTypeT_>
     struct OfS_f
     {
-    private:
-        enum { STATIC_ASSERT_IN_ENUM((!IsUseImmutableArray_f<UseArrayTypeS_>::V), MUST_NOT_BE_USE_IMMUTABLE_ARRAY) };
-    public:
-        typedef ImplementationOf_t<Diagonal2TensorProductOfBasedVectorSpaces_c<Factor1_,Factor2_>,
-                                   Scalar_,
-                                   UseArrayTypeS_> T;
+        typedef Diagonal2TensorProductOfBasedVectorSpaces_c<Factor1_,Factor2_> T;
     };
 
-    template <typename UseArrayTypeV_ = UseArrayTypeT_>
     struct OfV_f
     {
-    private:
-        enum { STATIC_ASSERT_IN_ENUM((!IsUseImmutableArray_f<UseArrayTypeV_>::V), MUST_NOT_BE_USE_IMMUTABLE_ARRAY) };
-    public:
-        typedef ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor2_,
-                                                                      TypeList_t<typename DualOf_f<Factor2_>::T > > >,
-                                   Scalar_,
-                                   UseArrayTypeV_> T;
+        typedef TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor2_,
+                                                   TypeList_t<typename DualOf_f<Factor2_>::T > > > T;
     };
 
-    template <typename UseArrayTypeSol_ = UseArrayTypeT_>
     struct SolutionVector_f
     {
-    private:
-      enum { STATIC_ASSERT_IN_ENUM((!IsUseImmutableArray_f<UseArrayTypeSol_>::V), MUST_NOT_BE_USE_IMMUTABLE_ARRAY) };
-    public:
-      typedef ImplementationOf_t<typename DualOf_f<Factor2_>::T, Scalar_, UseArrayTypeSol_> T;
+      typedef typename DualOf_f<Factor2_>::T T;
     };
 
-    template <typename UseArrayTypeConstantTerm_ = UseArrayTypeT_>
     struct ConstantTerm_f
     {
-      typedef ImplementationOf_t<Factor1_, Scalar_, UseArrayTypeConstantTerm_> T;
+      typedef Factor1_ T;
     };
 };
 
 template <typename T> struct EigenMatrixFor_f;
 
-template <typename Factor1_, typename Factor2_, typename Scalar_, typename UseArrayType_, typename Derived_>
-struct EigenMatrixFor_f<ImplementationOf_t<TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,TypeList_t<Factor2_> > >,Scalar_, UseArrayType_, Derived_> >
+template <typename Factor1_, typename Factor2_, typename Scalar_, typename Derived_, bool COMPONENTS_ARE_IMMUTABLE_>
+struct EigenMatrixFor_f<Tensor_i<Derived_, Scalar_, TensorProductOfBasedVectorSpaces_c<TypeList_t<Factor1_,TypeList_t<Factor2_> > >, COMPONENTS_ARE_IMMUTABLE_> >
 {
     typedef Eigen::Matrix<Scalar_,DimensionOf_f<Factor1_>::V,DimensionOf_f<Factor2_>::V,Eigen::RowMajor> T;
 };
 
 template <typename T> struct EigenVectorFor_f;
 
-template <typename Type_, typename Scalar_, typename UseArrayType_, typename Derived_>
-struct EigenVectorFor_f<ImplementationOf_t<Type_, Scalar_, UseArrayType_, Derived_> >
+template <typename Type_, typename Scalar_, typename Derived_, bool COMPONENTS_ARE_IMMUTABLE_>
+struct EigenVectorFor_f<Vector_i<Derived_, Scalar_, Type_, COMPONENTS_ARE_IMMUTABLE_> >
 {
     typedef Eigen::Matrix<Scalar_,DimensionOf_f<Type_>::V,1,Eigen::RowMajor> T;
 };
 
 
-template <typename TType_, typename UseArrayTypeU_ = UseMemberArray, typename UseArrayTypeS_ = UseMemberArray, typename UseArrayTypeV_ = UseMemberArray>
-void SVD_of_2tensor (TType_ const &t,
-                     typename SVDReturnTypesOf_m<TType_>::template OfU_f<UseArrayTypeU_>::T &u,
-                     typename SVDReturnTypesOf_m<TType_>::template OfS_f<UseArrayTypeS_>::T &s,
-                     typename SVDReturnTypesOf_m<TType_>::template OfV_f<UseArrayTypeV_>::T &v)
+template <typename DerivedT_, typename DerivedU_, typename DerivedS_, typename DerivedV_, typename Scalar_, typename Concept_, bool COMPONENTS_ARE_IMMUTABLE_>
+void SVD_of_2tensor (Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> const &t,
+                     Tensor_i<DerivedU_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfU_f::T, MUTABLE_COMPONENTS> &u,
+                     Tensor_i<DerivedS_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfS_f::T, MUTABLE_COMPONENTS> &s,
+                     Tensor_i<DerivedV_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfV_f::T, MUTABLE_COMPONENTS> &v)
 {
-    typedef typename EigenMatrixFor_f<TType_>::T EigenMatrix;
+    typedef typename EigenMatrixFor_f<Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> >::T EigenMatrix;
     Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeFullU|Eigen::ComputeFullV));
     memcpy(u.pointer_to_allocation(), &svd.matrixU()(0,0), u.allocation_size_in_bytes());
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
@@ -116,49 +93,47 @@ void SVD_of_2tensor (TType_ const &t,
 }
 
 // SVD with u matrix but no v matrix; the singular values are in s.
-template <typename TType_, typename UseArrayTypeU_ = UseMemberArray, typename UseArrayTypeS_ = UseMemberArray>
-void SVD_of_2tensor (TType_ const &t,
-                     typename SVDReturnTypesOf_m<TType_>::template OfU_f<UseArrayTypeU_>::T &u,
-                     typename SVDReturnTypesOf_m<TType_>::template OfS_f<UseArrayTypeS_>::T &s)
+template <typename DerivedT_, typename DerivedU_, typename DerivedS_, typename Scalar_, typename Concept_, bool COMPONENTS_ARE_IMMUTABLE_>
+void SVD_of_2tensor (Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> const &t,
+                     Tensor_i<DerivedU_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfU_f::T, MUTABLE_COMPONENTS> &u,
+                     Tensor_i<DerivedS_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfS_f::T, MUTABLE_COMPONENTS> &s)
 {
-    typedef typename EigenMatrixFor_f<TType_>::T EigenMatrix;
+    typedef typename EigenMatrixFor_f<Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> >::T EigenMatrix;
     Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeFullU));
     memcpy(u.pointer_to_allocation(), &svd.matrixU()(0,0), u.allocation_size_in_bytes());
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
 }
 
 // SVD with v matrix but no u matrix; the singular values are in s.
-template <typename TType_, typename UseArrayTypeS_ = UseMemberArray, typename UseArrayTypeV_ = UseMemberArray>
-void SVD_of_2tensor (TType_ const &t,
-                     typename SVDReturnTypesOf_m<TType_>::template OfS_f<UseArrayTypeS_>::T &s,
-                     typename SVDReturnTypesOf_m<TType_>::template OfV_f<UseArrayTypeV_>::T &v)
+template <typename DerivedT_, typename DerivedS_, typename DerivedV_, typename Scalar_, typename Concept_, bool COMPONENTS_ARE_IMMUTABLE_>
+void SVD_of_2tensor (Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> const &t,
+                     Tensor_i<DerivedS_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfS_f::T, MUTABLE_COMPONENTS> &s,
+                     Tensor_i<DerivedV_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfV_f::T, MUTABLE_COMPONENTS> &v)
 {
-    typedef typename EigenMatrixFor_f<TType_>::T EigenMatrix;
+    typedef typename EigenMatrixFor_f<Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> >::T EigenMatrix;
     Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeFullV));
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
     memcpy(v.pointer_to_allocation(), &svd.matrixV()(0,0), v.allocation_size_in_bytes());
 }
 
 // SVD with no u or v matrix; the singular values are in s.
-template <typename TType_, typename UseArrayTypeS_ = UseMemberArray>
-void SVD_of_2tensor (TType_ const &t,
-                     typename SVDReturnTypesOf_m<TType_>::template OfS_f<UseArrayTypeS_>::T &s)
+template <typename DerivedT_, typename DerivedU_, typename DerivedS_, typename DerivedV_, typename Scalar_, typename Concept_, bool COMPONENTS_ARE_IMMUTABLE_>
+void SVD_of_2tensor (Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> const &t,
+                     Tensor_i<DerivedS_, Scalar_, typename SVDReturnTypesOf_m<Concept_>::OfS_f::T, MUTABLE_COMPONENTS> &s)
 {
-    typedef typename EigenMatrixFor_f<TType_>::T EigenMatrix;
+    typedef typename EigenMatrixFor_f<Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> >::T EigenMatrix;
     Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd());
     memcpy(s.pointer_to_allocation(), &svd.singularValues()(0,0), s.allocation_size_in_bytes());
 }
 
-template <typename TType_, typename UseArrayTypeSol_ = UseMemberArray, typename UseArrayTypeConstantTerm_ = UseMemberArray>
-void SVD_solve (TType_ const &t,
-                typename SVDReturnTypesOf_m<TType_>::template ConstantTerm_f<UseArrayTypeConstantTerm_>::T const &b,
-                typename SVDReturnTypesOf_m<TType_>::template SolutionVector_f<UseArrayTypeSol_>::T &x)
+template <typename DerivedT_, typename DerivedB_, typename DerivedX_, typename Scalar_, typename Concept_, bool COMPONENTS_ARE_IMMUTABLE_, bool COMPONENTS_ARE_IMMUTABLE_b_>
+void SVD_of_2tensor (Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> const &t,
+                     Vector_i<DerivedB_, Scalar_, typename SVDReturnTypesOf_m<DerivedT_>::ConstantTerm_f::T, COMPONENTS_ARE_IMMUTABLE_b_> const &b,
+                     Vector_i<DerivedX_, Scalar_, typename SVDReturnTypesOf_m<DerivedT_>::SolutionVector_f::T, MUTABLE_COMPONENTS> &x)
 {
-  typedef typename SVDReturnTypesOf_m<TType_>::template ConstantTerm_f<UseArrayTypeConstantTerm_>::T ConstantTermType;
-  typedef typename SVDReturnTypesOf_m<TType_>::template SolutionVector_f<UseArrayTypeConstantTerm_>::T SolutionType;
-  typedef typename EigenMatrixFor_f<TType_>::T EigenMatrix;
-  typedef typename EigenVectorFor_f<ConstantTermType>::T ConstantTermVector;
-  typedef typename EigenVectorFor_f<SolutionType>::T SolutionVector;
+  typedef typename EigenMatrixFor_f<Tensor_i<DerivedT_, Scalar_, Concept_, COMPONENTS_ARE_IMMUTABLE_> >::T EigenMatrix;
+  typedef typename EigenVectorFor_f<Vector_i<DerivedB_, Scalar_, typename SVDReturnTypesOf_m<DerivedT_>::ConstantTerm_f::T, COMPONENTS_ARE_IMMUTABLE_b_> >::T ConstantTermVector;
+  typedef typename EigenVectorFor_f<Vector_i<DerivedX_, Scalar_, typename SVDReturnTypesOf_m<DerivedT_>::SolutionVector_f::T, MUTABLE_COMPONENTS> >::T SolutionVector;
 
   Eigen::JacobiSVD<EigenMatrix> svd(EigenMap_of_2tensor(t).jacobiSvd(Eigen::ComputeThinU|Eigen::ComputeThinV));
   EigenMap_of_vector(x) = svd.solve(EigenMap_of_vector(b));
