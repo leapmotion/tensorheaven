ccbe84df58e8ddde906fff02150a420d5f14014d
Ted Nitz <tnitz@leapmotion.com>
2013-12-31 13:30:42 -0800

    Make the generated tests a single make target because of whiny gods.

--- tensorhell/generate_compile_tests.pl
+++ tensorhell/generate_compile_tests.pl
@@ -7,22 +7,35 @@ my $compile_tests_dir = "compile_tests";
 my $source_directory = "../include/tenh";
 my @compile_test_files;
 
-process_directory($compile_tests_dir, $source_directory);
+if (!-d $compile_tests_dir)
+{
+    mkdir $compile_tests_dir;
+}
 
-open(my $fh, ">", "$compile_tests_dir/CMakeLists.txt");
+process_directory("$compile_tests_dir/tenh", $source_directory);
+
+open(my $fh, ">", "$compile_tests_dir/main.cpp");
+
+print $fh <<"EOT";
+int main(void)
+{
+    return 0;
+}
+EOT
+
+close($fh);
+
+open($fh, ">", "$compile_tests_dir/CMakeLists.txt");
+
+print $fh "set(compile_test_SRCS\nmain.cpp\n";
 
 foreach my $file (@compile_test_files)
 {
     $file =~ s/$compile_tests_dir\///;
-    my $filename = $file;
-    $filename =~ s/\/|\./_/g;
-    print $fh <<"EOT"
-set(${filename}_SRCS
-    $file)
-add_executable(${filename}_test \${${filename}_SRCS})
-
-EOT
+    print $fh "$file\n";
 }
+
+print $fh ")\nadd_executable(compile_test \${compile_test_SRCS})\n";
 
 close($fh);
 
@@ -31,12 +44,11 @@ sub process_directory
 {
     my $output_directory = shift;
     my $input_directory = shift;
+
     if (!-d $output_directory)
     {
         mkdir $output_directory;
     }
-
-
 
     opendir(my $dh, $input_directory) or die "Failed to open directory: $input_directory\n";
 
@@ -51,15 +63,17 @@ FILE:
         elsif ($file =~ /.hpp$/)
         {
             my $outfile = $file;
+            my $function_name = $file;
             "$input_directory/$file" =~ /(tenh\/.*)/;
             my $include_path = $1;
             $outfile =~ s/.hpp$/.cpp/;
+            $function_name =~ s/\/|\./_/;
 
             open(my $fh, ">", "$output_directory/$outfile") or die "Failed to open file: $output_directory/$outfile\n";
             print $fh <<"EOT";
 #include "$include_path"
 
-int main(void)
+int $function_name()
 {
     return 0;
 }
